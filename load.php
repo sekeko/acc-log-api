b3aa588b4f28b565d1108e3a44981eefdc38cb2f
sekeko <fernando.sequeira@gmail.com>
2015-07-13 07:21:33 -0600

    first add

---
+++ load.php
@@ -0,0 +1,10 @@
+<?php
+require 'core/JSONResponse.php';
+require 'core/Database.php';
+require 'core/Api.php';
+
+$mysql_user = "accu";
+$mysql_password = "HVejheDEE6AQbAtF";
+$mysql_host = "localhost";
+$mysql_database_name = "acc";
+?>
